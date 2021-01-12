cca8dd0654bdbc4395f06c278ee2b01ba727bada
Bhagat Aastha <109658@eesd.org>
2021-01-12 00:08:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a comment, comments are not run by the compiler
+# Output: Information that the computer prints out, what you see printed on the right hand side
+
+print("Hello World")
+
+# Name, grade, favorite book, and favorite superhero
+
+print("My name is Aastha Bhagat")
+print("my favorite book is 'scooby Doo'")
+print("my favorite superhero is BATMAN!")
+
+print("Hello")
+print("World")
+
+# Concatenation: add two strings together using '+'
+print("Hello" + " World")
+
+
+
